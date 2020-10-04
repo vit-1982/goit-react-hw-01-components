aaa63f1909ec3f780abd1fa8a72b54efa6d140d8
Vitalii Zelenyi <vzntst@gmail.com>
2020-10-04 19:07:24 +0300

    third commit

---
+++ build/precache-manifest.7565a44d411cc552b1b8834a06f9ae03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7329e68e17b6c4cada6214e6a352293",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "640f9da06c4b0f05dfac",
+    "url": "/goit-react-hw-01-components/static/css/main.da3fd194.chunk.css"
+  },
+  {
+    "revision": "7f707fc6e953494e8702",
+    "url": "/goit-react-hw-01-components/static/js/2.a718c2c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.a718c2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640f9da06c4b0f05dfac",
+    "url": "/goit-react-hw-01-components/static/js/main.a0279610.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vit-1982/goit-react-hw-01-components/precache-manifest.e2b49c09a2a40be530d1e71f9db267c1.js"
+  "/goit-react-hw-01-components/precache-manifest.7565a44d411cc552b1b8834a06f9ae03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vit-1982/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
